02c63bb4b0486e3df41181ca7f8105c85a2a1725
bakaryc50 <bakaryc50@gmail.com>
2022-02-13 14:15:06 +0000

    Bakary commit: 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Gives an employee's TODO list and write it to a file as a JSON
+Summarize an employee's TODO list and write it to a file as JSON
 """
 from argparse import ArgumentParser
-from csv import QUOTE_ALL, writer
+from json import dump
 from os import path
 from requests import get
 from sys import argv
@@ -19,8 +19,8 @@ if __name__ == '__main__':
     with open('.'.join([str(args.id), 'json']), 'w') as ostream:
         dump({
             str(args.id): [{
-		"task": task["title"],
-		"completed": task["completed"],
-		"username": user["username"],
+                "task": task['title'],
+                "completed": task['completed'],
+                "username": user['username'],
             } for task in get(TODOS, params={'userId': args.id}).json()]
-	}, ostream)
+        }, ostream)
